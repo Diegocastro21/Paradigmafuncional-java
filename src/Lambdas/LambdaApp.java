dcea8a2cbb5088999642c4a8186244f50da4f7ee
Diegocastro21 <Diegocamilocastrooliveros@gmail.com>
2022-05-29 11:14:35 -0500

    Practica con Lamda

---
+++ src/Lambdas/LambdaApp.java
@@ -0,0 +1,53 @@
+package Lambdas;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class LambdaApp {
+
+    public void ordenar(){
+        List<String> lista = new ArrayList<>();
+        lista.add("Manizales");
+        lista.add("Cartagena");
+        lista.add("Managua");
+        lista.add("Barranquilla");
+
+        /*Collections.sort(lista, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.compareTo(o2);
+            }
+        });
+*/
+        /*
+        * solo en una linea se remplazo el trabajo de 6 linas de codigo
+        * */
+        //Lambda
+        Collections.sort(lista, (String p1, String p2) -> p1.compareTo(p2));
+        for (String elemento:
+             lista) {
+            System.out.println(elemento);
+        }
+    }
+
+    public void calcular(){
+       /* Operacion operacion = new Operacion() {
+            @Override
+            public double calcularPromedio(double n1, double n2) {
+                return (n1+n2)/2;
+            }
+        };*/
+        //System.out.println(operacion.calcularPromedio(2, 3));
+
+        Operacion operacion = (double x, double y) -> (x+y)/2;
+        System.out.println(operacion.calcularPromedio(2, 3));
+    }
+
+    public static void main(String[] args) {
+        LambdaApp app = new LambdaApp();
+        app.ordenar();
+        app.calcular();
+    }
+}
